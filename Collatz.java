44b8347c0227508b069939e0daeb2bd683f05cb9
Sahar Zolberg <161973653+SaharZo321@users.noreply.github.com>
2024-11-11 20:43:21 +0000

    Answer to Collatz

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,30 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+		int limit = Integer.parseInt(args[0]);
+		boolean isVerbose = args[1].charAt(0) == 'v';
+
+		for (int i = 1; i <= limit; i++) {
+			String line = getSequence(i);
+			if (isVerbose) {
+				System.out.println(line);
+			}
+		}
+		System.out.printf("\nEvery one of the first %d hailstone sequences reached 1\n", limit);
+
+	}
+
+	public static String getSequence(int value) {
+		int counter = 2;
+		String line = value + " ";
+		value = value % 2 == 0 ? value / 2 : value * 3 + 1;
+		line += value + " ";
+		while (value != 1) {
+			value = value % 2 == 0 ? value / 2 : value * 3 + 1;
+			line += value + " ";
+			counter++;
+		}
+		line += "(" + counter + ")";
+		return line;
 	}
 }
