b853820644eb38044cfdff0eac3f8ac44c92be88
Sahar Zolberg <161973653+SaharZo321@users.noreply.github.com>
2024-11-17 20:05:45 +0000

    Cheers fix

--- Cheers.java
+++ Cheers.java
@@ -1,21 +1,13 @@
 // Prints a crowd cheering output.
 
-import java.util.HashSet;
-
 public class Cheers {
     public static void main(String[] args) {
         String word = args[0].toUpperCase();
         int counter = Integer.parseInt(args[1]);
-        char[] anCharsArray = { 'A', 'E', 'F', 'H', 'I', 'L', 'M', 'N', 'O', 'R', 'S', 'X' };
-        var anCharsSet = new HashSet<Character>();
-
-        for (char c : anCharsArray) {
-            anCharsSet.add(c);
-        }
-
+        String anChars = "AEFHILMNORSX";
         for (int i = 0; i < word.length(); i++) {
             char c = word.charAt(i);
-            if (anCharsSet.contains(c)) {
+            if (anChars.indexOf(c) != -1) {
                 System.out.printf("Give me an %c: %c!\n", c, c);
             } else {
                 System.out.printf("Give me a  %c: %c!\n", c, c);
