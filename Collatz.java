e52c370ba0e65c81463d5d7e971ea44e8201d64e
Sahar Zolberg <161973653+SaharZo321@users.noreply.github.com>
2024-11-17 22:27:56 +0000

    fix Collatz to be on method instead of 2

--- Collatz.java
+++ Collatz.java
@@ -5,26 +5,22 @@ public class Collatz {
 		boolean isVerbose = args[1].equals("v");
 
 		for (int i = 1; i <= limit; i++) {
-			String line = getSequence(i);
+			int counter = 2;
+			int value = i;
+			String line = value + " ";
+			value = value % 2 == 0 ? value / 2 : value * 3 + 1;
+			line += value + " ";
+			while (value != 1) {
+				value = value % 2 == 0 ? value / 2 : value * 3 + 1;
+				line += value + " ";
+				counter++;
+			}
+			line += "(" + counter + ")";
 			if (isVerbose) {
 				System.out.println(line);
 			}
 		}
 		System.out.printf("Every one of the first %d hailstone sequences reached 1.\n", limit);
 
-	}
-
-	public static String getSequence(int value) {
-		int counter = 2;
-		String line = value + " ";
-		value = value % 2 == 0 ? value / 2 : value * 3 + 1;
-		line += value + " ";
-		while (value != 1) {
-			value = value % 2 == 0 ? value / 2 : value * 3 + 1;
-			line += value + " ";
-			counter++;
-		}
-		line += "(" + counter + ")";
-		return line;
 	}
 }
